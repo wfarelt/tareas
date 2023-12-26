dd23dad31de837ef3c66f2666d891ade8f101ae5
wfarel <wfarel.t@gmail.com>
2023-12-26 16:58:33 -0400

    update tamplate task

--- task/views.py
+++ task/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from . import models
+from .models import Task
 from . import forms
 
 
 # Create your views here.
 
 def task_list(request):
-    tasks = models.Task.objects.all()
+    tasks = Task.objects.all()
     return render(request, 'task/task_list.html', {'tasks': tasks})
 
 def task_detail(request, pk):
-    task = get_object_or_404(models.Task, pk=pk)
+    task = get_object_or_404(Task, pk=pk)
     return render(request, 'task/task_detail.html', {'task': task})
 
 def task_create(request):
@@ -24,7 +24,7 @@ def task_create(request):
     return render(request, 'task/task_create.html', {'form': form})
 
 def task_edit(request, pk):
-    task = get_object_or_404(models.Task, pk=pk)
+    task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
         form = forms.TaskForm(request.POST, instance=task)
         if form.is_valid():
@@ -35,6 +35,6 @@ def task_edit(request, pk):
     return render(request, 'task/task_edit.html', {'form': form})
 
 def task_delete(request, pk):
-    task = get_object_or_404(models.Task, pk=pk)
+    task = get_object_or_404(Task, pk=pk)
     task.delete()
     return redirect('task:task_list')
