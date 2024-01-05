795ca24da7d70de9eb6f581b1691bb72128ddb3c
wfarel <wfarel.t@gmail.com>
2024-01-05 17:24:21 -0400

    add SuccessMessageMixin

--- cmp/views.py
+++ cmp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin as SMM
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Proveedor
@@ -13,33 +14,37 @@ class ProveedorView(LoginRequiredMixin, ListView):
     context_object_name = 'obj'
     login_url = 'bases:login'
 
-class ProveedorNew(LoginRequiredMixin, CreateView):
+class ProveedorNew(SMM, LoginRequiredMixin, CreateView):
     model = Proveedor
     template_name = 'cmp/proveedor_form.html'
     context_object_name = 'obj'
     form_class = ProveedorForm
     success_url = reverse_lazy('cmp:proveedor_list')
     login_url = 'bases:login'
-
+    success_message = 'Proveedor creado exitosamente.'
+   
     def form_valid(self, form):
         form.instance.uc = self.request.user
         return super().form_valid(form)
 
-class ProveedorEdit(LoginRequiredMixin, UpdateView):
+class ProveedorEdit(SMM, LoginRequiredMixin, UpdateView):
     model = Proveedor
     template_name = 'cmp/proveedor_form.html'
     context_object_name = 'obj'
     form_class = ProveedorForm
     success_url = reverse_lazy('cmp:proveedor_list')
     login_url = 'bases:login'
-
+    success_message = 'Proveedor actualizado exitosamente.'
+    
     def form_valid(self, form):
         form.instance.um = self.request.user.id
         return super().form_valid(form)
 
-class ProveedorDel(LoginRequiredMixin, DeleteView):
+class ProveedorDel(SMM, LoginRequiredMixin, DeleteView):
     model = Proveedor
     template_name = 'cmp/catalogos_del.html'
     context_object_name = 'obj'
     success_url = reverse_lazy('cmp:proveedor_list')
     extra_context = {'clase': 'Proveedor', 'pag_anterior': 'cmp:proveedor_list'}
+    login_url = 'bases:login'
+    success_message = 'Proveedor eliminado exitosamente.'
